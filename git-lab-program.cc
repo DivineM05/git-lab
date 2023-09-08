26b5bf6206fc2891661c2c5575135f628ed74293
DivineM05 <dm472622@ohio.edu>
2023-09-08 16:17:14 -0400

    git lab program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,15 @@
+/*
+File: git-lab-program.cc
+Author: Divine Mbabazi
+Date: Friday Sep 8, 2023 
+Description: For lab 2
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main (int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+return 0;
+}
